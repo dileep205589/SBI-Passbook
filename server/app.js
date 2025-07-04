5b85d455ce770b3998873908b3762b6f2ad9e947
Dileep Meena <dileep205589@gmail.com>
2025-07-04 15:02:39 +0530

    live link

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(fileUpload({
 
 // CORS Setup
 app.use(cors({
-  origin: "http://localhost:3000", // React frontend origin
+  origin: "https://glittery-rugelach-213d0f.netlify.app", // React frontend origin
   credentials: true
 }));
 
